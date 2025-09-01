a83a7d0ead2182f6fd0184a746da3b0a5b7cb710
Dinesh Sherpa <itprodinesh3@outlook.com>
2025-09-01 11:59:15 +0100

    variable placement

--- script.js
+++ script.js
@@ -3,12 +3,6 @@
 const API_URL = "https://api.nasa.gov/planetary/apod";
 const API_KEY = "gNDqlB62a1CVhBH4VRrcjxOXo77gK6J6eZ6UB9hn";
 
-// Random date generator 0 to 9
-let randomYear = 2015 + Math.floor(Math.random() * 10);
-
-let tempDate = Math.floor(Math.random() * 10);
-let newDate = randomYear + "-06-1" + tempDate;
-
 // Testing code
 //console.log(newDate);
 
@@ -19,8 +13,16 @@ const button = document.getElementById("image_button");
 // Function to get image from NASA APOD - Astronomy Picture of the Day
 async function getNasaImage() {
   try {
+    
+    // Random date generator 0 to 9
+    let randomYear = 2015 + Math.floor(Math.random() * 10);
+
+    let tempDate = Math.floor(Math.random() * 10);
+    let newDate = randomYear + "-06-1" + tempDate;
     //const response = await fetch(`https://api.unsplash.com/photos/random?client_id=${API_KEY}&date=` );
-    const response = await fetch(`${API_URL}?api_key=${API_KEY}&date=${newDate}`);
+    const response = await fetch(
+      `${API_URL}?api_key=${API_KEY}&date=${newDate}`
+    );
 
     const data = await response.json();
 
@@ -30,7 +32,6 @@ async function getNasaImage() {
 
     imageElement.src = imageUrl; // Update the src of the image element
     imageDescription.textContent = imageExplanation; // Update the content of the paragraph element
-    
   } catch (error) {
     console.error("Error fetching image:", error);
   }
@@ -38,12 +39,3 @@ async function getNasaImage() {
 
 // Event listener to trigger image update
 button.addEventListener("click", getNasaImage);
-
-
-
-
-
-
-
-
-
