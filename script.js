a29ece2fe1b0ad6debd21445c94f1fb1665ce17b
Dinesh Sherpa <itprodinesh3@outlook.com>
2025-09-01 13:35:50 +0100

    updates

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
+// Query parameters
+// Define the API URL
+const API_URL = "https://api.nasa.gov/planetary/apod";
+const API_KEY = "gNDqlB62a1CVhBH4VRrcjxOXo77gK6J6eZ6UB9hn";
+
+const imageElement = document.getElementById("external_image");
+const imageDescription = document.getElementById("external_image_description");
+const button = document.getElementById("image_button");
+
 // Function to get image from NASA APOD - Astronomy Picture of the Day
 async function getNasaImage() {
   try {
-    // Query parameters
-    // Define the API URL
-    const API_URL = "https://api.nasa.gov/planetary/apod";
-    const API_KEY = "gNDqlB62a1CVhBH4VRrcjxOXo77gK6J6eZ6UB9hn";
-
-    const imageElement = document.getElementById("external_image");
-    const imageDescription = document.getElementById("external_image_description");
-
     // Random year (2015 to 2024) and date generator (0 to 9)
     // Used to display random images
     let randomYear = 2015 + Math.floor(Math.random() * 10);
@@ -16,7 +17,9 @@ async function getNasaImage() {
     let newDate = randomYear + "-06-1" + tempDate;
 
     // API Call
-    const response = await fetch(`${API_URL}?api_key=${API_KEY}&date=${newDate}`);
+    const response = await fetch(
+      `${API_URL}?api_key=${API_KEY}&date=${newDate}`
+    );
     const data = await response.json();
 
     //Using the JSON response to update image src and description
