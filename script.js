6ec25310249ff541219ffd6babfde3b19d19e341
Dinesh Sherpa <itprodinesh3@outlook.com>
2025-09-01 12:13:13 +0100

    updates

--- script.js
+++ script.js
@@ -1,49 +1,32 @@
-// // Query parameters
-// // Define the API URL
-// const API_URL = "https://api.nasa.gov/planetary/apod";
-// const API_KEY = "gNDqlB62a1CVhBH4VRrcjxOXo77gK6J6eZ6UB9hn";
+// Query parameters
+// Define the API URL
+const API_URL = "https://api.nasa.gov/planetary/apod";
+const API_KEY = "gNDqlB62a1CVhBH4VRrcjxOXo77gK6J6eZ6UB9hn";
 
-// // Testing code
-// //console.log(newDate);
-
-// const imageElement = document.getElementById("external_image");
-// const imageDescription = document.getElementById("external_image_description");
-// const button = document.getElementById("image_button");
-
+const imageElement = document.getElementById("external_image");
+const imageDescription = document.getElementById("external_image_description");
 
 // Function to get image from NASA APOD - Astronomy Picture of the Day
 async function getNasaImage() {
   try {
-    // Query parameters
-    // Define the API URL
-    const API_URL = "https://api.nasa.gov/planetary/apod";
-    const API_KEY = "gNDqlB62a1CVhBH4VRrcjxOXo77gK6J6eZ6UB9hn";
 
-    // Testing code
-    //console.log(newDate);
-
-    const imageElement = document.getElementById("external_image");
-    const imageDescription = document.getElementById("external_image_description");
-    //const button = document.getElementById("image_button");
-
-    // Random date generator 0 to 9
+    // Random year (2015 to 2024) and date generator (0 to 9)
+    // Used to display random images
     let randomYear = 2015 + Math.floor(Math.random() * 10);
-
     let tempDate = Math.floor(Math.random() * 10);
     let newDate = randomYear + "-06-1" + tempDate;
-    //const response = await fetch(`https://api.unsplash.com/photos/random?client_id=${API_KEY}&date=` );
-    const response = await fetch(
-      `${API_URL}?api_key=${API_KEY}&date=${newDate}`
-    );
 
+    // API Call
+    const response = await fetch(`${API_URL}?api_key=${API_KEY}&date=${newDate}`);
     const data = await response.json();
 
-    //const imageUrl = data[0].urls.regular; // Get the URL of the image
-    const imageUrl = data["url"]; // Get the URL of the image
-    const imageExplanation = data["explanation"];
+    //Using the JSON response to update image src and description
+    const imageUrl = data["url"]; // Get the URL of the image 
+    const imageExplanation = data["explanation"]; // Get the image description text
 
     imageElement.src = imageUrl; // Update the src of the image element
     imageDescription.textContent = imageExplanation; // Update the content of the paragraph element
+
   } catch (error) {
     console.error("Error fetching image:", error);
   }
