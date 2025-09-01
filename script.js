c7554d5a440731181c4377cc13ff1bd1b26a6a17
Dinesh Sherpa <itprodinesh3@outlook.com>
2025-09-01 16:30:26 +0100

    almost ready

--- script.js
+++ script.js
@@ -1,37 +1,51 @@
-// Query parameters
-// Define the API URL
+// API details
 const API_URL = "https://api.nasa.gov/planetary/apod";
 const API_KEY = "gNDqlB62a1CVhBH4VRrcjxOXo77gK6J6eZ6UB9hn";
 
+// Constants used to refence various HTML elements for modifications
 const imageElement = document.getElementById("external_image");
+const imageDate = document.getElementById("external_image_date");
 const imageDescription = document.getElementById("external_image_description");
-const button = document.getElementById("image_button");
+const imageButton = document.getElementById("image_button");
+const searchDate = document.getElementById("search_date");
+const searchButton = document.getElementById("search_button");
+
+// Generate a random date which determies the next random image to display
+// Random year (2015 to 2024) and date generator (0 to 9)
+function getRandomDate() {
+  let randomYear = 2015 + Math.floor(Math.random() * 10);
+  let tempDate = Math.floor(Math.random() * 10);
+  let newDate = randomYear + "-06-1" + tempDate;
+  getNasaImage(newDate);
+}
+
+// Use user supplied date to lookup specific image published on that date
+function userSearchDate() {
+  let newDate = searchDate.value;
+  getNasaImage(newDate);
+}
 
 // Function to get image from NASA APOD - Astronomy Picture of the Day
-async function getNasaImage() {
+async function getNasaImage(newDate) {
   try {
-    // Random year (2015 to 2024) and date generator (0 to 9)
-    // Used to display random images
-    let randomYear = 2015 + Math.floor(Math.random() * 10);
-    let tempDate = Math.floor(Math.random() * 10);
-    let newDate = randomYear + "-06-1" + tempDate;
 
     // API Call
-    const response = await fetch(
-      `${API_URL}?api_key=${API_KEY}&date=${newDate}`
-    );
+    const response = await fetch(`${API_URL}?api_key=${API_KEY}&date=${newDate}`);
     const data = await response.json();
 
-    //Using the JSON response to update image src and description
-    const imageUrl = data["url"]; // Get the URL of the image
-    const imageExplanation = data["explanation"]; // Get the image description text
+    //Using the JSON response to update image src, date, description
+    const sourceImageUrl = data["url"]; // Get the URL of the image
+    const sourceImageExplanation = data["explanation"]; // Get the image description text
+    const sourceImageDate = data["date"]; // Get the image date
 
-    imageElement.src = imageUrl; // Update the src of the image element
-    imageDescription.textContent = imageExplanation; // Update the content of the paragraph element
+    imageElement.src = sourceImageUrl; // Update the src of the image element
+    imageDescription.textContent = "Explanation: " + sourceImageExplanation; // Update the content of the paragraph element
+    imageDate.textContent = "Date: " + sourceImageDate; // Update date
   } catch (error) {
     console.error("Error fetching image:", error);
   }
 }
 
-// Event listener to trigger image update
-button.addEventListener("click", getNasaImage);
+// Event listeners to trigger image update
+imageButton.addEventListener("click", getRandomDate);
+searchButton.addEventListener("click", userSearchDate);
